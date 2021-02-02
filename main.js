db20cdd51adecef9bdfae63860fc242d2c596e78
thuanbasto <thuanbasto113@gmail.com>
2021-02-02 12:46:58 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const btnthaphuong = document.getElementsByClassName("btnthaphuong");
+const btnlay = document.getElementsByClassName("btnlay")
+
+const gifThaphuong = document.getElementsByClassName("gifThaphuong");
+const gifLay = document.getElementsByClassName("gifLay");
+
+
+function showGif() {
+    // console.log(event.target === btnlay[0]);
+    // console.log(event.target === btnthaphuong[0]);
+    if (event.target === btnlay[0]){
+        gifLay[0].setAttribute('style', 'display:inline !important');
+        setTimeout(() => {
+            gifLay[0].setAttribute('style', 'display:none !important');
+        }, 3000);
+    } else if (event.target === btnthaphuong[0]) {
+        gifThaphuong[0].setAttribute('style', 'display:inline !important');
+        setTimeout(() => {
+            gifThaphuong[0].setAttribute('style', 'display:none !important');
+        }, 3000);
+    }
+}
