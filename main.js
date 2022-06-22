bf6e2b57fbe4f4ac06feb6865eb645bdf62a13b6
thuanbasto <45193209+thuanbasto@users.noreply.github.com>
2022-06-22 14:55:04 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const btnlay = document.getElementsByClassName("btnlay")
 
 const gifThaphuong = document.getElementsByClassName("gifThaphuong");
 const gifLay = document.getElementsByClassName("gifLay");
-
+document.getElementsByTagName("audio")[0].play();
 
 function showGif() {
     // console.log(event.target === btnlay[0]);
